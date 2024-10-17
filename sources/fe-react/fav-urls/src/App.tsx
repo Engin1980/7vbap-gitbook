9964b0a5720b9dc22fb63f9403b54e19b551ec9d
Marek Vajgl <engin@seznam.cz>
2024-10-17 22:06:42 +0200

    Add Url-Edit component.

--- sources/fe-react/fav-urls/src/App.tsx
+++ sources/fe-react/fav-urls/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
 import UrlList from "./components/url/url-list";
+import UrlEdit from "./components/url/url-edit";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import {ToastContainer} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-       FAVOURITE URLs
-      </header>
-      <UrlList />
+    <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+      />
+
+      <div className="App">
+        <header className="App-header">
+          FAVOURITE URLs
+        </header>
+        <UrlList/>
+        <UrlEdit/>
+      </div>
     </div>
   );
 }
