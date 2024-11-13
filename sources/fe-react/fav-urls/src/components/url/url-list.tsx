a202f6db801fe7482ea414aa37987f5524290b72
Marek Vajgl <engin@seznam.cz>
2024-11-13 11:56:16 +0100

    Update url-list with new DTO location.

--- sources/fe-react/fav-urls/src/components/url/url-list.tsx
+++ sources/fe-react/fav-urls/src/components/url/url-list.tsx
@@ -1,35 +1,40 @@
 import {useEffect, useState} from "react";
-import {UrlView} from "../../model/dtos/url-view";
-import axios from "axios";
+import {UrlView} from "../../model/url-view";
 import "./url-list.css";
 import UrlCreatePopup from "./url-create-popup";
 import useHttp from "../../hooks/use-http";
+import {useLoggedUser} from "../../hooks/use-logged-user";
+import {toast} from "react-toastify";
 
-function UrlList(){
+function UrlList() {
   const [urls, setUrls] = useState<UrlView[]>([]);
   const [refresh, setRefresh] = useState<boolean>(true);
   const http = useHttp();
-  const doRefresh : () => void =  () => setRefresh(true);
+  const doRefresh: () => void = () => setRefresh(true);
+  const {loggedUser} = useLoggedUser();
+
+  const appUserId = loggedUser?.appUserId ?? 0;
 
   useEffect(() => {
     (async () => {
-      if (refresh){
+      if (refresh) {
         setRefresh(false);
         try {
-          const res = await http.get<UrlView[]>("/v1/url/1");
+          const res = await http.get<UrlView[]>(`/v1/url/${appUserId}`);
           setUrls(res);
         } catch (err) {
           console.error(err);
+          toast.error("Failed to get data.");
         }
       }
     })();
-  }, [refresh]);
+  }, [refresh, http, appUserId]);
 
   return (
     <div>
       <h1>Your Links</h1>
       <div>
-        <UrlCreatePopup appUserId={1} refresh={doRefresh} />
+        <UrlCreatePopup appUserId={1} refresh={doRefresh}/>
         {!urls && <div>Loading...</div>}
         {urls && urls.map(url => (<div className="urlRow" key={url.urlId}>
           <div className="urlTitle">{url.title}</div>
